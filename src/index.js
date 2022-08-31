de0e66fd0248e908a955dd2a6b99dd53ecbb591c
Caio Lucas <ccaio.oliveira83@gmail.com>
2022-08-31 19:11:16 -0300

    iniciando projeto

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+
+root.render(
+    <>
+        <h1>OOOOOi</h1>
+    </>
+)
