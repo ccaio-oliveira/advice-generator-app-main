986413a8324316fe8c031efd1e1091b8c15f96e0
Caio Lucas <ccaio.oliveira83@gmail.com>
2022-09-07 21:52:33 -0300

    adicionando atributo alt

--- src/components/buttons/index.jsx
+++ src/components/buttons/index.jsx
@@ -6,8 +6,8 @@ const Buttons = ({changeAdvice}) => {
 
     return(
         <div id="btn">
-            <button id="stopBtn"><img src='./images/pattern-divider-mobile.svg' /></button>
-            <button id="startBtn" onClick={() => {changeAdvice()}}><img src="./images/icon-dice.svg" /></button>
+            <button id="stopBtn"><img src='./images/pattern-divider-mobile.svg' alt='image' /></button>
+            <button id="startBtn" onClick={() => {changeAdvice()}}><img src="./images/icon-dice.svg" alt='image' /></button>
         </div>
     )
 }
